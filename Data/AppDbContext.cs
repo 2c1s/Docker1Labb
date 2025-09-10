23acf67e6458155843d9d55fc75bb1bd1da87ccc
Yosef Safi <yosef.safi@iths.se>
2025-09-10 05:24:44 +0200

    rohneyek

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Docker1Labb.Models;
+
+namespace Docker1Labb.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext
+{
+    public DbSet<TodoItem> Todos => Set<TodoItem>();
+
+   
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            var cs =
+               
+                Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection")
+                
+                ?? "Server=localhost;Database=LabDb;User Id=sa;Password=Your_strong@Passw0rd;TrustServerCertificate=True;Encrypt=False";
+
+            optionsBuilder.UseSqlServer(cs);
+        }
+    }
+}
